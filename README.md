5580dd04ec810601aeda880afb1a84222f3c713d
Freewind <nowindlee@gmail.com>
2021-11-12 20:13:50 +0800

    java-indico-client-list-workflows-demo

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-Java Hello World Demo
+Java Indico Client List Workflows Demo
 =====================
 
-Use maven for Java.
+List workflows from [Indico](https://indicodata.ai/) with Java client.
 
-Run `Hello.java` in your IDE.
+Set host and Indico api token in `config.properties`, and run:
+
+```
+./run.sh
+```
+
+You will see the fetched workflows.
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,34 @@
 package demo;
 
+import com.indico.IndicoConfig;
+import com.indico.IndicoKtorClient;
+import com.indico.query.ListWorkflows;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+
 public class Hello {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static IndicoConfig loadConfig() throws IOException {
+        var properties = new Properties();
+        properties.load(new FileReader("config.properties"));
+
+        return new IndicoConfig.Builder()
+                .host(properties.getProperty("host"))
+                .apiToken(properties.getProperty("indicoToken"))
+                .build();
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (var client = new IndicoKtorClient(loadConfig())) {
+            var listWorkflows = new ListWorkflows(client);
+            var workflows = listWorkflows.query();
+            for (var workflow : workflows) {
+                System.out.printf("workflow: id='%s' name='%s' reviewEnabled='%s'\n", workflow.id, workflow.name, workflow.reviewEnabled);
+            }
+        }
     }
 
 }
